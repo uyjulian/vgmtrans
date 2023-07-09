081733029e1d70d8819cd09b7f424e79926972c9
mikelow <mikelow@users.noreply.github.com>
2023-07-08 20:00:59 -0700

    * integrate NewSequencePlayer (to be renamed) into SequencePlayer. NewSequencePlayer handles actual midi/audio playback and SequencePlayer serves as the Qt middleware * add seek logic to NewSequencePlayer * add a callback to indicate when a SF2 was loaded by NewSequencePlayer, allowing playback immediately thereafter * go back to sending SF2 files to the VST via a single request. The multi-request transfer was a workaround for named pipes, which we are not using any more

--- src/ui/qt/workarea/VGMCollListView.cpp
+++ src/ui/qt/workarea/VGMCollListView.cpp
@@ -12,7 +12,7 @@
 #include <QObject>
 #include <VGMColl.h>
 #include <VGMExport.h>
-#include "NewSequencePlayer.h"
+#include "SequencePlayer.h"
 #include "QtVGMRoot.h"
 
 static const QIcon &VGMCollIcon() {
@@ -172,9 +172,9 @@ void VGMCollListView::handlePlaybackRequest() {
   }
 
   VGMColl *coll = qtVGMRoot.vVGMColl[list[0].row()];
-  NewSequencePlayer::the().playCollection(coll);
+  SequencePlayer::the().playCollection(coll);
 }
 
 void VGMCollListView::handleStopRequest() {
-  NewSequencePlayer::the().stop();
+  SequencePlayer::the().stop();
 }
